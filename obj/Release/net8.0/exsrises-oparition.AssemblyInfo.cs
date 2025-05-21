a59fde7ea58866cc1cfe1c13ac63119a89fe1ce2
Netanel kalir <netanelkalir@gmail.com>
2025-05-21 11:42:14 +0300

    i bild a class uman

--- obj/Release/net8.0/exsrises-oparition.AssemblyInfo.cs
+++ obj/Release/net8.0/exsrises-oparition.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("exsrises-oparition")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6d313b94d8924a3258273af493a60974cfca19")]
 [assembly: System.Reflection.AssemblyProductAttribute("exsrises-oparition")]
