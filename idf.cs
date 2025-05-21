a194cc18acf0d81c74784e0c36174fe65996ea06
Netanel kalir <netanelkalir@gmail.com>
2025-05-21 12:15:38 +0300

    idf class

---
+++ idf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IDFProgect
+{
+    public class idf:Military-organization
+    {
+        private List<string> AttackCapabilities = new List(string[] capabilities);
+
+
+        public idf(string name , int date , string commander, LIst<string> attackCapabilities ) :base(name, date, commander)
+        {
+            AttackCapabilities = attackCapabilities;
+        }
+
+    }
+}
+
+
