ea56c289d92b5299198a6cfa6e3fb6a5c9d91c87
Netanel kalir <netanelkalir@gmail.com>
2025-05-21 12:20:47 +0300

    Hamas class

---
+++ Hamas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IDFProgect
+{
+    public class Hamas : Military-organization
+    {
+        private List<string> ListoOfTerrorists = new List(string[] terrorists);
+
+
+        public Hamas(string name, int date, string commander, LIst<string> listOfTerrorists) : base(name, date, commander)
+        {
+            ListoOfTerrorists = listOfTerrorists;
+        }
+
+    }
+}
